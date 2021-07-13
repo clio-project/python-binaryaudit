9fa55ec82a11200b2a0bc567517a2d4aa8b14559
Anatol Belski <anbelski@linux.microsoft.com>
2021-07-13 18:47:30 +0000

    setup: Fix name, add cli executable example
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup
 
-setup(name = "binaryoudit",
+setup(name = "binaryaudit",
         version = "0.0.2",
         description = "ELF binary audit",
         url = "",
@@ -9,5 +9,10 @@ setup(name = "binaryoudit",
         author_email = "anbelski@linux.microsoft.com",
         license = "MIT",
         packages = ["binaryaudit"],
+        install_requires = [
+        ],
+        scripts = [
+            "bin/ba_example",
+        ],
         zip_safe = False)
 
