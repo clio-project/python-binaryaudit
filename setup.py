5a007da2e1c83069a502529d5cfd54ad0a08c85d
Anatol Belski <anbelski@linux.microsoft.com>
2021-07-13 18:37:05 +0000

    structure: Added basic setup.py
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name = "binaryoudit",
+        version = "0.0.2",
+        description = "ELF binary audit",
+        url = "",
+        author = "Anatol Belski",
+        author_email = "anbelski@linux.microsoft.com",
+        license = "MIT",
+        packages = ["binaryaudit"],
+        zip_safe = False)
+
