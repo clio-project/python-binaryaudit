6d4927795402e4f4e2855d45658d93c195768d3f
Anatol Belski <anbelski@linux.microsoft.com>
2021-07-14 19:45:40 +0000

    flake8: Fix warnings
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 
 from setuptools import setup
 
-setup(name = "binaryaudit",
-        version = "0.0.2",
-        description = "ELF binary audit",
-        url = "",
-        author = "Anatol Belski",
-        author_email = "anbelski@linux.microsoft.com",
-        license = "MIT",
-        packages = ["binaryaudit"],
-        install_requires = [
-        ],
-        scripts = [
-            "bin/ba_is_elf",
-        ],
-        zip_safe = False)
-
+setup(name="binaryaudit",
+      version="0.0.2",
+      description="ELF binary audit",
+      url="",
+      author="Anatol Belski",
+      author_email="anbelski@linux.microsoft.com",
+      license="MIT",
+      packages=["binaryaudit"],
+      install_requires=[
+      ],
+      scripts=[
+        "bin/ba_is_elf",
+      ],
+      zip_safe=False)
