89655604457e976ba18ac7eca1fd77bd861946ec
Anatol Belski <anbelski@linux.microsoft.com>
2021-07-27 11:42:25 +0000

    db_orchestrator: Revamp args for object creation
    The required options are only to be checked, when telemetry
    option has been passed.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- binaryaudit/cli.py
+++ binaryaudit/cli.py
@@ -1,6 +1,8 @@
 
 import argparse
 
+from . import util
+
 # Top level arg parser.
 arg_parser = argparse.ArgumentParser(prog="binaryaudit", description="Tools for ELF audit.")
 
@@ -8,9 +10,21 @@ arg_parser = argparse.ArgumentParser(prog="binaryaudit", description="Tools for 
 arg_parser.add_argument("--is-elf", action="store", metavar="/path/to/elf/file",
                         help="Determine whether a file is an ELF artifact. Exit is zero if true.")
 
-arg_parser.add_argument('-t', '--enable-telemetry', action='store', required=False,
+telemetry_args = arg_parser.add_argument_group('telemetry arguments')
+telemetry_args.add_argument('-t', '--enable-telemetry', action='store', required=False,
                         default="n", help="Enable the data storing to telemetry (default: n)")
-arg_parser.add_argument('-v', '--verbose', action='store_true',
+telemetry_args.add_argument('-b', '--build-id', action='store', required=False,
+                           help="Reference to the particular build.")
+telemetry_args.add_argument('-d', '--product-name', action='store', required=False,
+                           help="Name of the product to be tracked, e.g. a distro or a project name.")
+telemetry_args.add_argument('-e', '--derivative', action='store', required=False,
+                           help="Derivative name")
+telemetry_args.add_argument('-u', '--buildurl', action='store', required=False,
+                           help="Build URL")
+telemetry_args.add_argument('-l', '--logurl', action='store', required=False,
+                           help="URL of log files")
+
+telemetry_args.add_argument('-v', '--verbose', action='store_true',
                         help="Verbose output.")
 
 # If a subcommand has been called, read the name from args.cmd
@@ -33,14 +47,10 @@ arg_parser_rpm_list.add_argument('--out-filename', action="store", help="Output 
 
 # binaryaudit db ..
 arg_parser_db = arg_parser_subs.add_parser("db", help="Database CLI wrapper.")
-required_args = arg_parser_db.add_argument_group('mandatory arguments')
-required_args.add_argument('-b', '--build-id', action='store', required=True,
-                           help="Reference to the particular build.")
-required_args.add_argument('-d', '--product-name', action='store', required=True,
-                           help="Name of the product to be tracked, e.g. a distro or a project name.")
-required_args.add_argument('-e', '--derivative', action='store', required=True,
-                           help="Derivative name")
-arg_parser_db.add_argument('-u', '--buildurl', action='store', required=False,
-                           help="Build URL")
-arg_parser_db.add_argument('-l', '--logurl', action='store', required=False,
-                           help="URL of log files")
+
+##### functions #####
+
+def validate_telemetry_args(args):
+    if args.enable_telemetry:
+        if not args.build_id or not args.product_name or not args.derivative:
+            util.fatal("With --enable-telemetry the options --build-id, --product-name and --derivative become required")
