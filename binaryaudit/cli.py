db3524a8245a1e15144445b1a5294a0cb3e0634e
Anatol Belski <anbelski@linux.microsoft.com>
2021-08-05 14:46:17 +0000

    binaryaudit: Basic implementation to compare and save telemetry
    Integration with the orchestrator is to go.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

--- binaryaudit/cli.py
+++ binaryaudit/cli.py
@@ -46,7 +46,7 @@ arg_parser_subs = arg_parser.add_subparsers(help="Subcommands", dest="cmd")
 
 
 # binaryaudit rpm ...
-arg_parser_rpm = arg_parser_subs.add_parser("rpm", help="RPM tools.",
+arg_parser_rpm = arg_parser_subs.add_parser("rpm", help="RPM tools frontend.",
                                             parents=[arg_parser_common, arg_parser_db, arg_parser_telemetry])
 arg_parser_rpm.add_argument('--list', action="store_true",
                             help="Read RPM packages in a directory and create a list grouped by SRPM.")
@@ -59,6 +59,15 @@ arg_parser_db_cmd = arg_parser_subs.add_parser("db", help="Database CLI wrapper.
                                                parents=[arg_parser_common, arg_parser_db])
 arg_parser_db_cmd.add_argument('--check-connection', action='store_true', required=False,
                                help="Test DB connection. Exit with 0 if connection could be established.")
+
+
+# binaryaudit poky ...
+arg_parser_poky = arg_parser_subs.add_parser("poky", help="RPM tools frontend.",
+                                             parents=[arg_parser_common, arg_parser_db, arg_parser_telemetry])
+arg_parser_poky.add_argument("--compare-buildhistory", action="store_true", help="Run abicompat on two buildhistory dirs.")
+arg_parser_poky.add_argument("--buildhistory-baseline", action="store", help="Baseline buildhistory directory.")
+arg_parser_poky.add_argument("--buildhistory-current", action="store",
+                             help="Current buildhistory directory to be compared against the baseline.")
 
 
 # ##### functions #####
