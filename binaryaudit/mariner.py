691afdb0a336762de5f440c6c327281250b19c1b
Angelina <vuangelina72@gmail.com>
2021-08-12 11:12:53 -0700

    Update mariner.py

--- binaryaudit/mariner.py
+++ binaryaudit/mariner.py
@@ -6,4 +6,4 @@ def binary_audit(source_dir, output_dir, build_id, product_id, db_conn):
     new_json_file = conf.get_config("Mariner", "new_json_file_name")
     old_json_file = conf.get_config("Mariner", "old_json_file_name")
     abicheck.generate_package_json(source_dir, new_json_file)
-    dnf.process_downloads(source_dir, new_json_file, old_json_file, output_dir, build_id, product_id, db_conn
+    dnf.process_downloads(source_dir, new_json_file, old_json_file, output_dir, build_id, product_id, db_conn)
